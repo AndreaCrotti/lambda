41a94b938dcaa321553a61b78fabfdb883c7617f
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-11-09 14:22:37 +0000

    cleanup

--- code/classes.py
+++
@@ -1,7 +0,0 @@
-class Inc(object):
-    def __init__(self):
-        self.value = 0
-
-    def inc(self):
-        self.value += 1
-        return self.value
--- code/tree.hs
+++
@@ -1,6 +0,0 @@
-module Tree where
-
-data Tree a = Leaf a | Branch (Tree a) (Tree a) deriving (Show, Eq)
-
-traverse :: (Tree a) -> (a -> b) -> (Tree a)
-traverse (Leaf x) func = Leaf x
