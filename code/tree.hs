33d2a8d5d7e94e72cfeedb969646f0b234959ea0
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-12-06 09:55:26 +0000

    better output

--- code/tree.hs
+++ code/tree.hs
@@ -15,10 +15,10 @@ find_gifts (Leaf gift) = [gift]
 find_gifts (Branch left right) = (find_gifts left) ++ (find_gifts right)
 
 instance (Show a) => Show (ChristmasTree a) where
-  show Nil = "Nothing"
-  show (Leaf gift) = show gift
+  show Nil = "O"
+  show (Leaf gift) = "G"
   show (Branch left right) = "*\n" ++ (show left) ++ " ~~ " ++ (show right)
   
 main :: IO ()
 main = do
-  print $ Branch (Leaf "Toy") (Branch (Leaf "Ball") Nil)
+  print $ Branch (Leaf "Toy") (Branch (Leaf "Ball") (Leaf "Toy"))
