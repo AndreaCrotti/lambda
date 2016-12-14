ce49bb4b9693b452d5936cf85f6f45fa2bd4dfed
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-12-14 14:36:15 +0000

    types example

--- code/my_types.py
+++ code/my_types.py
@@ -5,10 +5,14 @@ from collections import Counter
 def enrich(text: str) -> Counter:
     return Counter(text)
 
+def typed_addition(a: int, b: int) -> int:
+    return a + b
+
 
 if __name__ == '__main__':
     print(enrich("Hello world"))
     print(enrich(100))
+    typed_addition(1, "string")
 
 # Local Variables:
 # compile-command: "mypy --silent-imports types.py"
